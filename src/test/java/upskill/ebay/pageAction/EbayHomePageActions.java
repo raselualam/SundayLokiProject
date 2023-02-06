512f11eec524335f5366c75a84938160eb7ae071
raselualam <raselualam@gmail.com>
2023-02-05 20:06:46 -0500

    3rd

--- src/test/java/upskill/ebay/pageAction/EbayHomePageActions.java
+++ src/test/java/upskill/ebay/pageAction/EbayHomePageActions.java
@@ -1,6 +1,12 @@
 package upskill.ebay.pageAction;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import upskill.ebay.pageElements.EbayHomePageLocators;
 import upskill.utilities.SetupDrivers;
@@ -31,7 +37,41 @@ public class EbayHomePageActions {
 	
 	public void searchItems(String items){
 		EbayHomePageLocatorsObj.txtbxSearch.sendKeys(items);
-		EbayHomePageLocatorsObj.btnSearch.click();
+//		EbayHomePageLocatorsObj.btnSearch.click();
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.ENTER);
 	}
 	
+	public void synchronization(){
+		/*	Selenium Wait : 
+				1. Implicit wait(Global), 
+				2. Explicit wait(Conditional), 
+				3. Fluent wait(intermittent) */
+			
+		//Implicit Wait
+		SetupDrivers.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		//Explicit Wait
+		WebDriverWait explicitWait = new WebDriverWait(SetupDrivers.driver, 10);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(EbayHomePageLocatorsObj.btnSearch));
+		
+		//Fluent Wait
+		FluentWait fluentWait = new FluentWait(SetupDrivers.driver);								
+		fluentWait.withTimeout(10, TimeUnit.SECONDS);
+		fluentWait.pollingEvery(2, TimeUnit.SECONDS);
+	}
+	
+	public void keyboardKeys(){
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.ENTER);
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.UP);
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.DOWN);
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.CLEAR);
+		EbayHomePageLocatorsObj.btnSearch.sendKeys(Keys.DELETE);
+	}
+	
+	public void handleAlert(){
+		SetupDrivers.driver.switchTo().alert().accept();
+		SetupDrivers.driver.switchTo().alert().dismiss();
+		SetupDrivers.driver.switchTo().alert().sendKeys("Good service");
+		SetupDrivers.driver.switchTo().alert().getText();
+	}
 }
